8dee571cd4e3498451b90fe46f5b8e810ea562ed
Kamil <blueflamesxcode@gmail.com>
2017-08-14 14:24:52 +0200

    Add ServiceRepository file with it's public class, and architecture without implementation

---
+++ src/enigma/ServiceRepository.java
@@ -0,0 +1,37 @@
+package enigma;
+
+import registry.ServiceRegistry;
+import registry.ServiceProvider;
+import services.EnigmaService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ServiceRepository implements ServiceRegistry, ServiceProvider {
+    List<EnigmaService> services;
+
+    public ServiceRepository()
+    {
+		this.services = new ArrayList<EnigmaService>();
+
+	}
+
+	public void register(EnigmaService service)
+    {
+
+	}
+
+	public List<String> listAll()
+    {
+
+        return null;
+	}
+
+	public EnigmaService getByName(String name)
+    {
+
+        return null;
+	}
+
+}
