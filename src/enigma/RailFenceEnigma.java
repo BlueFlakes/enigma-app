41eb1f6b0e3658cc56c91d20403a64ed39b144e1
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-17 14:15:24 +0200

    Implement RailFenceEnigma

---
+++ src/enigma/RailFenceEnigma.java
@@ -0,0 +1,104 @@
+package enigma;
+
+import services.EnigmaService;
+import enigma.exceptions.WrongKeyException;
+
+public class RailFenceEnigma implements EnigmaService {
+
+    public static final boolean KEY_REQUIRED = true;
+    private static Integer key;
+	public static final String NAME = "RailFenceEnigma";
+
+    public RailFenceEnigma() {
+	}
+
+	public String encipher(String text){
+        String[] lines = splitIntoLines(text, false);
+        String encipheredText = "";
+
+        for (String line : lines) {
+            encipheredText += line;
+        }
+
+        return encipheredText;
+	}
+
+	public String decipher(String text) {
+        String[] lines = splitIntoLines(text, true);
+        String decipheredText;
+
+        /* replace ?s with the actual letter */
+        int charIndex = 0;
+        for (int i = 0; i < key; ++i) {
+            while (lines[i].contains("?")) {
+                String letter = String.valueOf(text.charAt(charIndex));
+                lines[i] = lines[i].replaceFirst("\\?", letter);
+                charIndex++;
+            }
+        }
+
+        decipheredText = condenseToString(lines, text);
+
+        return decipheredText;
+	}
+
+	public String getName(){
+		return NAME;
+	}
+
+	public boolean isKeyRequired(){
+		return KEY_REQUIRED;
+	}
+
+	public void setKey(String s) {
+		try {
+			if (CaesarEnigma.isInteger(s)) {
+	            key = Integer.parseInt(s) % 26;
+	        }
+		} catch (WrongKeyException e) {
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+    }
+
+    private static String[] splitIntoLines(String text, boolean enciphered) {
+        String[] result = generateEmptyStrings();
+        int lineIndex = 0;
+        int direction = -1;
+
+        for (char c : text.toCharArray()) {
+            String letter = String.valueOf(c);
+            /* if the message is already encrypted, use '?' as placeholder */
+            result[lineIndex] += enciphered ? "?" : letter;
+            direction *= (lineIndex == 0 || lineIndex == key - 1) ? -1 : 1;
+            lineIndex += direction;
+        }
+        return result;
+    }
+
+    private static String[] generateEmptyStrings() {
+        String[] strings = new String[key];
+        for (int i = 0; i < key; ++i) {
+            strings[i] = "";
+        }
+        return strings;
+    }
+
+    private static String condenseToString(String[] lines, String text) {
+
+        String decipheredText = "";
+        int lineIndex = 0;
+        int direction = -1;
+
+        for (int i = 0; i < text.length(); ++i) {
+            /* Add first letter to decipheredText by removing it from the line */
+            String letter = String.valueOf(lines[lineIndex].charAt(0));
+            lines[lineIndex] = lines[lineIndex].substring(1);
+            decipheredText += letter;
+            direction *= (lineIndex == 0 || lineIndex == key - 1) ? -1 : 1;
+            lineIndex += direction;
+        }
+
+        return decipheredText;
+    }
+}
