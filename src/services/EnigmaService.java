dd6266ce289c128e9956ed633f8988fa6051566c
Maciej Nowak <mcnowak@wp.pl>
2017-08-17 13:33:29 +0200

    Change EnigmaService interface to throw WrongKeyException in setKey method

--- src/services/EnigmaService.java
+++ src/services/EnigmaService.java
@@ -1,9 +1,10 @@
 package services;
+import enigma.exceptions.WrongKeyException;
 
 public interface EnigmaService {
     public String encipher(String text);
     public String decipher(String text);
     public String getName();
     public boolean isKeyRequired();
-    public void setKey(String key);
+    public void setKey(String key) throws WrongKeyException;
 }
